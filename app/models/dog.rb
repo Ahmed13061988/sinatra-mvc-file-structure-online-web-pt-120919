51ec4850bb3c7b6fce68564993a32da05c799f87
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-17 00:26:01 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+ @@all =[]
+attr_accessor :name, :breed, :age 
+
+  def initialize 
+   @name = name 
+   @breed = breed 
+   @age = age
+   @@all << self 
+  end  
+  def self.all 
+    @@all 
+  end 
+ 
+
+
+end 
